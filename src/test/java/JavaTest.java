3bebb5774565e1a32024bfaf7bdfabb069f17436
saahilclaypool <saahil@claypools.org>
2017-01-18 19:48:51 -0500

    init files

---
+++ src/test/java/JavaTest.java
@@ -0,0 +1,34 @@
+import junit.framework.*; 
+import org.junit.Test; 
+import org.junit.Ignore; 
+import static org.junit.Assert.assertEquals; 
+
+
+public class JavaTest extends TestCase {
+    protected int value1, value2; 
+
+    // run every test
+    protected void setUp() {
+        value1 = 1; 
+        value2 = 1; 
+    }
+
+    @Test
+    public void testAdd() {
+        double res = value1 + value2; 
+        assertTrue(res == 2); 
+    }
+
+    @Test
+    public void testFail() {
+        double res = value1 + value2; 
+        assertTrue(res == 3); 
+    }
+
+    @Test
+    public void testIfThisWorks() {
+        assertTrue(true);
+    }
+}
+
+
